364ae8f9e83362511b0f177989416396058ecaa6
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-05-30 12:54:13 +0200

    bonus 2 completion

--- js-snack-bonus/main.js
+++ js-snack-bonus/main.js
@@ -2,9 +2,15 @@ let repeat = 3
 let  single_number = null
 cube_number = null
 
-for (i = repeat; i >= 1; i--){
-    single_number = Number(prompt("Inserisci un numero"))
-    cube_number = single_number**3
+// for (i = repeat; i >= 1; i--){
+//     single_number = Number(prompt("Inserisci un numero"))
+//     cube_number = single_number**3
     
+//     console.log(cube_number);
+// }
+
+for(i = 0; cube_number<=1000; i++){
+    cube_number = 2**i
     console.log(cube_number);
+    
 }
