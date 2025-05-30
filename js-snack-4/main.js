fab59f5b50e842e0930505f4e061c8dcbcc4a6c5
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-05-30 12:31:57 +0200

    variables name debug

--- js-snack-4/main.js
+++ js-snack-4/main.js
@@ -1,8 +1,8 @@
-let reapet = 6
+let repeat = 6
 let numbers = []
 let  single_number = null
 
-for (i = reapet; i >= 1; i--){
+for (i = repeat; i >= 1; i--){
     single_number = Number(prompt("Inserisci un numero"))
     if(single_number % 2 !== 0){
 
