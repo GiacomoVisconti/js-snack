c4e8519afe7c361d735ee44d97f22738639e3d70
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-05-30 12:16:44 +0200

    alternative completion

--- js-snack-3/main.js
+++ js-snack-3/main.js
@@ -1,20 +1,28 @@
 //dichairo l'array da riepire col ciclo for
-let numbers = []
-let sum = null
-let  single_number = null
+// let numbers = []
+// let sum = null
+// let  single_number = null
 
-for (i = 10; i >= 1; i--){
-    single_number = Number(prompt("Inserisci un numero"))
+// for (i = 10; i >= 1; i--){
+//     single_number = Number(prompt("Inserisci un numero"))
 
-    numbers.push(single_number)
+//     numbers.push(single_number)
     
-}
-console.log(numbers);
+// }
+// console.log(numbers);
 
-for (i = 0; i < numbers.length; i++){
-    let element = numbers[i];
-    sum = sum + element
-}
+// for (i = 0; i < numbers.length; i++){
+//     let element = numbers[i];
+//     sum = sum + element
+// }
+// console.log(sum);
+let repeat = 5
+sum = null
 
+for(i = repeat; i >= 1; i--){
+    let single_number = Number(prompt("Inserisci un numero")) 
+    sum = sum + single_number
+
+}
 
 console.log(sum);
