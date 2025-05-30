e2e5027d53082789dd1fc8a0c5abb474f9a7373e
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-05-30 11:30:50 +0200

    add comments

--- js-snack-2/main.js
+++ js-snack-2/main.js
@@ -3,11 +3,13 @@ let parola_2  = prompt("Inserisci la seconda parola")
 
 //controllo della lunghezza delle parole
 if (parola_1.length <  parola_2.length){
-    //stampo prima la parola più lunga
+    //stampo prima la parola più corta
     alert(parola_1 + " " + parola_2)
 
 } else if(parola_1.length > parola_2.length){
+    //stampo prima la parola più corta
     alert(parola_2 + " " + parola_1)
 }else{
+    //stampo che le parole sono uguali
     alert("la lunghezza delle due parole è uguale")
 }
