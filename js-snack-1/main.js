7a48ba99caa8f29d7f59cbc01ced53759eebd205
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-05-30 10:47:13 +0200

    snack 1 completion

---
+++ js-snack-1/main.js
@@ -0,0 +1,8 @@
+let first_number = Number(prompt("Inserisci il primo numero"))
+let second_number = Number(prompt("Inserisci il secondo numero"))
+
+if(first_number > second_number){
+    alert(first_number)
+} else if(second_number > first_number){
+    alert(second_number)
+}
